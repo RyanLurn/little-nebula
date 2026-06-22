45a6a6ff5f51cb749eb797f5f506c6c3ea9541f4
Ryan Luong <lumintriet@gmail.com>
2026-06-22 18:57:13 +0700

    feat(ai/chat): add more ui elements.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
 import type { KeyboardEvent } from "react";
 
+import {
+  MessageSquare,
+  Loader2Icon,
+  SquareIcon,
+  SendIcon,
+  XIcon,
+  File,
+} from "lucide-react";
 import { createFileRoute } from "@tanstack/react-router";
-import { MessageSquare } from "lucide-react";
 import { useChat } from "@ai-sdk/react";
 import { useState } from "react";
 
@@ -17,6 +24,7 @@ import {
   Message,
 } from "@/components/ai-elements/message";
 import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: HomePage,
@@ -24,7 +32,7 @@ export const Route = createFileRoute("/")({
 
 function HomePage() {
   const [input, setInput] = useState("");
-  const { messages, sendMessage } = useChat();
+  const { messages, sendMessage, status } = useChat();
 
   function handleKeyDown(e: KeyboardEvent<HTMLTextAreaElement>) {
     if (e.key === "Enter") {
@@ -38,6 +46,16 @@ function HomePage() {
         form.requestSubmit();
       }
     }
+  }
+
+  let Icon = <SendIcon className="size-4" />;
+
+  if (status === "submitted") {
+    Icon = <Loader2Icon className="size-4 animate-spin" />;
+  } else if (status === "streaming") {
+    Icon = <SquareIcon className="size-4" />;
+  } else if (status === "error") {
+    Icon = <XIcon className="size-4" />;
   }
 
   return (
@@ -79,7 +97,7 @@ function HomePage() {
           void sendMessage({ text: input });
           setInput("");
         }}
-        className="mx-auto mb-3 w-full max-w-2xl overflow-hidden rounded-xl border bg-background shadow-sm"
+        className="mx-auto mb-3 w-full max-w-2xl divide-y-2 overflow-hidden rounded-xl border bg-background shadow-sm"
       >
         <Textarea
           className="field-sizing-content max-h-[6lh] w-full resize-none rounded-none border-none bg-transparent p-3 shadow-none ring-0 outline-none focus-visible:ring-0 dark:bg-transparent"
@@ -88,6 +106,23 @@ function HomePage() {
           onKeyDown={handleKeyDown}
           value={input}
         />
+        <div className="flex items-center justify-between p-1">
+          <div className="flex items-center gap-1 [&_button:first-child]:rounded-bl-xl">
+            <Button variant="outline" size="icon" disabled>
+              <File />
+            </Button>
+          </div>
+          <Button
+            disabled={
+              !input || status === "submitted" || status === "streaming"
+            }
+            className="gap-1.5 rounded-lg"
+            type="submit"
+            size="icon"
+          >
+            {Icon}
+          </Button>
+        </div>
       </form>
     </div>
   );
